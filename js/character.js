3699f8e26fa5cabe6544f61dd3e64f77ace6657a
usagi <woshawott@gmail.com>
2025-04-06 23:00:11 -0500

    image update

--- js/character.js
+++ js/character.js
@@ -38,6 +38,13 @@ export class Character {
         ];
     
         elements.forEach((el, i) => el.textContent = values[i]);
+
+        const imageContainer = document.querySelector("#character-image");    
+        imageContainer.innerHTML = "";
+        
+        const img = document.createElement("img");
+        img.src = this.profile.img;   
+        imageContainer.appendChild(img);
     }
     
     #updateInventoryHTML() {
